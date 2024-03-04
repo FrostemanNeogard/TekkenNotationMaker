13425cf8481d1e283061f7a12b327da7e896e99c
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-04 17:22:05 +0100

    Create object with all images and values

---
+++ src/__util/ImagePaths.ts
@@ -0,0 +1,238 @@
+// ARROWS
+import db from "../assets/images/upscaled/arrows/db.png";
+import d from "../assets/images/upscaled/arrows/d.png";
+import df from "../assets/images/upscaled/arrows/df.png";
+import b from "../assets/images/upscaled/arrows/b.png";
+import n from "../assets/images/upscaled/arrows/n.png";
+import f from "../assets/images/upscaled/arrows/f.png";
+import ub from "../assets/images/upscaled/arrows/ub.png";
+import u from "../assets/images/upscaled/arrows/u.png";
+import uf from "../assets/images/upscaled/arrows/uf.png";
+
+import db_h from "../assets/images/upscaled/arrows/db_h.png";
+import d_h from "../assets/images/upscaled/arrows/d_h.png";
+import df_h from "../assets/images/upscaled/arrows/df_h.png";
+import b_h from "../assets/images/upscaled/arrows/b_h.png";
+import f_h from "../assets/images/upscaled/arrows/f_h.png";
+import ub_h from "../assets/images/upscaled/arrows/ub_h.png";
+import u_h from "../assets/images/upscaled/arrows/u_h.png";
+import uf_h from "../assets/images/upscaled/arrows/uf_h.png";
+import bracket_l from "../assets/images/upscaled/arrows/bracket_left.png";
+import bracket_r from "../assets/images/upscaled/arrows/bracket_right.png";
+import next from "../assets/images/upscaled/arrows/next.png";
+
+// BUTTONS
+import one from "../assets/images/upscaled/buttons/1.png";
+import two from "../assets/images/upscaled/buttons/2.png";
+import three from "../assets/images/upscaled/buttons/3.png";
+import four from "../assets/images/upscaled/buttons/4.png";
+import onetwo from "../assets/images/upscaled/buttons/12.png";
+import onethree from "../assets/images/upscaled/buttons/13.png";
+import onefour from "../assets/images/upscaled/buttons/14.png";
+import twothree from "../assets/images/upscaled/buttons/23.png";
+import twofour from "../assets/images/upscaled/buttons/24.png";
+import threefour from "../assets/images/upscaled/buttons/34.png";
+import onetwothree from "../assets/images/upscaled/buttons/123.png";
+import onetwofour from "../assets/images/upscaled/buttons/124.png";
+import onethreefour from "../assets/images/upscaled/buttons/134.png";
+import twothreefour from "../assets/images/upscaled/buttons/234.png";
+import onetwothreefour from "../assets/images/upscaled/buttons/1234.png";
+
+// STAGE GIMMICKS
+import balcony from "../assets/images/upscaled/stage/balconybreak.png";
+import floorblast from "../assets/images/upscaled/stage/floorblast.png";
+import floorbreak from "../assets/images/upscaled/stage/floorbreak.png";
+import wallblast from "../assets/images/upscaled/stage/wallblast.png";
+import wallbound from "../assets/images/upscaled/stage/wallbound.png";
+import wallbreak from "../assets/images/upscaled/stage/wallbreak.png";
+
+// MISC
+import heatdash from "../assets/images/upscaled/misc/heatdash.png";
+import tornado from "../assets/images/upscaled/misc/tornado.png";
+import heatburst from "../assets/images/upscaled/misc/heatburst.png";
+
+export const ImagePaths = {
+  // ARROWS
+  db: {
+    text: "d/b",
+    src: db,
+  },
+  d: {
+    text: "d",
+    src: d,
+  },
+  df: {
+    text: "d/f",
+    src: df,
+  },
+  db_h: {
+    text: "D/B",
+    src: db_h,
+  },
+  d_h: {
+    text: "D",
+    src: d_h,
+  },
+  df_h: {
+    text: "D/F",
+    src: df_h,
+  },
+  b: {
+    text: "b",
+    src: b,
+  },
+  n: {
+    text: "n",
+    src: n,
+  },
+  f: {
+    text: "f",
+    src: f,
+  },
+  ub: {
+    text: "u/b",
+    src: ub,
+  },
+  u: {
+    text: "u",
+    src: u,
+  },
+  uf: {
+    text: "u/f",
+    src: uf,
+  },
+  b_h: {
+    text: "B",
+    src: b_h,
+  },
+  f_h: {
+    text: "F",
+    src: f_h,
+  },
+  ub_h: {
+    text: "U/B",
+    src: ub_h,
+  },
+  u_h: {
+    text: "U",
+    src: u_h,
+  },
+  uf_h: {
+    text: "U/F",
+    src: uf_h,
+  },
+  bracket_l: {
+    text: "[",
+    src: bracket_l,
+  },
+  bracket_r: {
+    text: "]",
+    src: bracket_r,
+  },
+  next: {
+    text: "->",
+    src: next,
+  },
+
+  // BUTTONS
+  one: {
+    text: "1",
+    src: one,
+  },
+  two: {
+    text: "2",
+    src: two,
+  },
+  three: {
+    text: "3",
+    src: three,
+  },
+  four: {
+    text: "4",
+    src: four,
+  },
+  onetwo: {
+    text: "1+2",
+    src: onetwo,
+  },
+  onethree: {
+    text: "1+3",
+    src: onethree,
+  },
+  onefour: {
+    text: "1+4",
+    src: onefour,
+  },
+  twothree: {
+    text: "2+3",
+    src: twothree,
+  },
+  twofour: {
+    text: "2+4",
+    src: twofour,
+  },
+  threefour: {
+    text: "3+4",
+    src: threefour,
+  },
+  onetwothree: {
+    text: "1+2+3",
+    src: onetwothree,
+  },
+  onetwofour: {
+    text: "1+2+4",
+    src: onetwofour,
+  },
+  onethreefour: {
+    text: "1+3+4",
+    src: onethreefour,
+  },
+  twothreefour: {
+    text: "2+3+4",
+    src: twothreefour,
+  },
+  onetwothreefour: {
+    text: "1+2+3+4",
+    src: onetwothreefour,
+  },
+
+  // STAGE
+  balcony: {
+    text: "BB!",
+    src: balcony,
+  },
+  floorblast: {
+    text: "Floorblast?",
+    src: floorblast,
+  },
+  floorbreak: {
+    text: "FB!",
+    src: floorbreak,
+  },
+  wallblast: {
+    text: "Wallblast?",
+    src: wallbound,
+  },
+  wallbound: {
+    text: "Wallbound?",
+    src: wallblast,
+  },
+  wallbreak: {
+    text: "WB!",
+    src: wallbreak,
+  },
+
+  // MISC
+  heatdash: {
+    text: "HD!",
+    src: heatdash,
+  },
+  tornado: {
+    text: "T!",
+    src: tornado,
+  },
+  heatburst: {
+    text: "HB!",
+    src: heatburst,
+  },
+};
