aa6d08a76bfa1ccabf584e33bf7f6b2eac4dbb46
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-06 17:30:09 +0100

    Add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./__App/App.index.tsx";
 import "./main.scss";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./__util/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
