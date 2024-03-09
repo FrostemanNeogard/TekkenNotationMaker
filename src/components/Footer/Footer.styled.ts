9bbaedbc8b44c1a26579867720b072b29ad5b500
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-10 00:20:29 +0100

    Increase font size and weight, and reduce padding on footer

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -3,8 +3,9 @@ import styled from "styled-components";
 export const Footer = styled.footer`
   background-color: ${(props) => props.theme.colors.secondary};
   border-top: 1px solid black;
-  padding: 2rem;
-  font-size: 1.2rem;
+  padding: 1.5rem;
+  font-size: 1.4rem;
+  font-weight: 500;
   text-align: center;
   a,
   a:visited {
