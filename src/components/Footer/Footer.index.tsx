0f016df9057cfc8c262f6790a4d811548cf6ddf3
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-09 23:40:02 +0100

    Use translations

--- src/components/Footer/Footer.index.tsx
+++ src/components/Footer/Footer.index.tsx
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next";
 import * as S from "./Footer.styled";
 
 export const Footer = () => {
+  const { t } = useTranslation("common");
+
   return (
     <S.Footer>
       <p>
-        App created by{" "}
+        {t("app.footer_start")}
         <a href="https://twitter.com/funnyorangcat" target="_blank">
           @funnyorangcat
         </a>
-        . If you like what I do, consider supporting me here:{" "}
+        {t("app.footer_end")}
         <a href="https://www.buymeacoffee.com/funnyorangcat" target="_blank">
           https://www.buymeacoffee.com/funnyorangcat
         </a>
