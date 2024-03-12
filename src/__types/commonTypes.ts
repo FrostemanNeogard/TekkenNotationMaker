a2e30a0813af23f51369616d649f084d263c5735
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-12 22:35:28 +0100

    Move all types into "commonTypes" file

---
+++ src/__types/commonTypes.ts
@@ -0,0 +1,20 @@
+export type NotationImage = {
+  text: string;
+  src: string;
+};
+
+export type CharacterNotationImage = {
+  [key: string]: {
+    [key: string]: NotationImage;
+  };
+};
+
+export type Character = { displayName: string; pathName: string };
+
+export type Theme =
+  | "arcade"
+  | "tekken8"
+  | "tekken7"
+  | "tekken8xbox"
+  | "tekken8playstation"
+  | "numpad";
--- src/__types/theme.ts
+++
@@ -1,7 +0,0 @@
-export type Theme =
-  | "arcade"
-  | "tekken8"
-  | "tekken7"
-  | "tekken8xbox"
-  | "tekken8playstation"
-  | "numpad";
