4253703b055662033e57012950dd381af92a8c3b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2026-04-04 23:59:28 +0200

    fix hosting url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TekkenNotationMaker/",
+  base: "/",
   server: {
     host: true,
   },
